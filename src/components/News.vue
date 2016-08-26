c3e7165ef5d58c99ed83337d471e6df7b7651671
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2016-08-26 11:44:16 +0800

    updated

--- src/components/News.vue
+++ src/components/News.vue
@@ -2,7 +2,7 @@
   <div class="ui text four item menu">
     <div class="ui item toggle checkbox">
       <input type="checkbox" id="checkbox0" v-model="showTime">
-      <label for="checkbox0">顯示時間</label>
+      <label for="checkbox0">顯示新聞時間</label>
     </div>
     <div class="ui item toggle checkbox">
       <input type="checkbox" id="checkbox1" v-model="hideLinkUnderline">
@@ -10,13 +10,16 @@
     </div>
     <div class="ui item toggle checkbox">
       <input type="checkbox" id="checkbox2" v-model="hideShortUrl">
-      <label for="checkbox2">移除短連結</label>
+      <label for="checkbox2">移除短連結網址</label>
     </div>
     <div class="ui item toggle checkbox">
       <input type="checkbox" id="checkbox3" v-model="hideTextLink">
-      <label for="checkbox3">移除文字連結</label>
+      <label for="checkbox3">移除文字超連結</label>
     </div>
   </div>
+
+  <div class="ui horizontal divider header">今天的消防新聞</div>
+
   <div class="ui piled segment fixed">
     <div id="news">
       <div v-if="loading" class="ui active inverted dimmer">
@@ -39,8 +42,32 @@
       </ol>
     </div>
   </div>
+
+  <div class="ui horizontal divider header">明天的消防新聞</div>
+
+  <div class="ui piled segment fixed">
+    <div id="news">
+      <div v-if="loading" class="ui active inverted dimmer">
+        <div class="ui large loader"></div>
+      </div>
+      <span v-if="!loading">{{ nextday }} 消防新聞</span>
+      <ol>
+        <li v-for="item in items3 | orderBy 'time'">
+          <a v-if="!hideTextLink" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.originLink }}" target="_blank">{{ item.title }}</a><span v-if="hideTextLink">{{ item.title }}</span>（{{ item.source }}）<span v-if="showTime">{{ item.timeText }}</span>
+          <br/><a v-if="!hideShortUrl" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.link }}" target="_blank">{{ item.link }}</a>
+        </li>
+      </ol>
+      <br />
+      <span v-if="!loading">{{ nextday }} 消防新聞（續）</span>
+      <ol>
+        <li v-for="item in items4 | orderBy 'time'">
+          <a v-if="!hideTextLink" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.originLink }}" target="_blank">{{ item.title }}</a><span v-if="hideTextLink">{{ item.title }}</span>（{{ item.source }}）<span v-if="showTime">{{ item.timeText }}</span>
+          <br/><a v-if="!hideShortUrl" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }"href="{{ item.link }}" target="_blank">{{ item.link }}</a>
+        </li>
+      </ol>
+    </div>
+  </div>
   <small class="ui horizontal inverted divider header">
-    <i class="tag icon"></i>
     lancetw&lt;at&gt;gmail.com, 2016
   </small>
 </template>
@@ -56,15 +83,23 @@ export default {
     this.loading = true
     this.$http.get(serverAddress + '/api/news/v1/main').then((response) => {
       const rdata = sortBy(response.data.news, (o) => { return o.time })
+      const items1 = pickBy(rdata, (o) => {
+        return moment(o.time).isBetween(moment().subtract(1, 'day').hour(17).minute(55), moment().hour(5).minute(55), 'minute', '[)')
+      })
       const items2 = pickBy(rdata, (o) => {
         return moment(o.time).isBetween(moment().hour(5).minute(55), moment().hour(17).minute(55), 'minute', '[)')
       })
-      const items1 = pickBy(rdata, (o) => {
-        return moment(o.time).isBetween(moment().subtract(1, 'day').hour(17).minute(55), moment().hour(5).minute(55), 'minute', '[)')
+      const items3 = pickBy(rdata, (o) => {
+        return moment(o.time).isBetween(moment().hour(17).minute(55), moment().add(1, 'day').hour(5).minute(55), 'minute', '[)')
+      })
+      const items4 = pickBy(rdata, (o) => {
+        return moment(o.time).isBetween(moment().add(1, 'day').hour(5).minute(55), moment().add(1, 'day').hour(17).minute(55), 'minute', '[)')
       })
 
       this.items1 = items1
       this.items2 = items2
+      this.items3 = items3
+      this.items4 = items4
 
       this.loading = false
     }, (errors) => {
@@ -79,8 +114,11 @@ export default {
       hideShortUrl: false,
       hideTextLink: false,
       today: moment().format('YYYY/MM/DD'),
+      nextday: moment().add(1, 'day').format('YYYY/MM/DD'),
       items1: [],
-      items2: []
+      items2: [],
+      items3: [],
+      items4: []
     }
   }
 }
