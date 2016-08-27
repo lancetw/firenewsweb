86bc9a78c4e46172151006912cba7c0a956e899d
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2016-08-27 14:55:25 +0800

    updated new segments

--- src/components/News.vue
+++ src/components/News.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="ui text four item menu">
     <div class="ui item toggle checkbox">
-      <input type="checkbox" id="checkbox0" v-model="showTime">
-      <label for="checkbox0">顯示新聞時間</label>
+      <input type="checkbox" id="checkbox0" v-model="hideTime">
+      <label for="checkbox0">隱藏時間</label>
     </div>
     <div class="ui item toggle checkbox">
       <input type="checkbox" id="checkbox1" v-model="hideLinkUnderline">
-      <label for="checkbox1">取消連結底線</label>
+      <label for="checkbox1">取消底線</label>
     </div>
     <div class="ui item toggle checkbox">
       <input type="checkbox" id="checkbox2" v-model="hideShortUrl">
-      <label for="checkbox2">移除短連結網址</label>
+      <label for="checkbox2">移除短連結</label>
     </div>
     <div class="ui item toggle checkbox">
       <input type="checkbox" id="checkbox3" v-model="hideTextLink">
-      <label for="checkbox3">移除文字超連結</label>
+      <label for="checkbox3">移除文字連結</label>
     </div>
   </div>
 
@@ -25,16 +25,16 @@
       <div v-if="loading" class="ui active inverted dimmer">
         <div class="ui large loader"></div>
       </div>
-      <span v-if="!loading">{{ today }} 消防新聞</span>
+      <p v-if="!loading">{{ today }} 消防新聞</p>
       <ol>
         <li v-for="item in items1 | orderBy 'time'">
-          <a v-if="!hideTextLink" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.originLink }}" target="_blank">{{ item.title }}</a><span v-if="hideTextLink">{{ item.title }}</span>（{{ item.source }}）<span v-if="showTime">{{ item.timeText }}</span>
+          <a v-if="!hideTextLink" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.originLink }}" target="_blank">{{ item.title }}</a><span v-if="hideTextLink">{{ item.title }}</span>（{{ item.source }}）<span v-if="!hideTime">{{ item.timeText }}</span>
           <br/><a v-if="!hideShortUrl" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.link }}" target="_blank">{{ item.link }}</a>
         </li>
       <br />
       <p v-if="!loading">{{ today }} 消防新聞（續）</p>
         <li v-for="item in items2 | orderBy 'time'">
-          <a v-if="!hideTextLink" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.originLink }}" target="_blank">{{ item.title }}</a><span v-if="hideTextLink">{{ item.title }}</span>（{{ item.source }}）<span v-if="showTime">{{ item.timeText }}</span>
+          <a v-if="!hideTextLink" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.originLink }}" target="_blank">{{ item.title }}</a><span v-if="hideTextLink">{{ item.title }}</span>（{{ item.source }}）<span v-if="!hideTime">{{ item.timeText }}</span>
           <br/><a v-if="!hideShortUrl" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }"href="{{ item.link }}" target="_blank">{{ item.link }}</a>
         </li>
       </ol>
@@ -48,21 +48,57 @@
       <div v-if="loading" class="ui active inverted dimmer">
         <div class="ui large loader"></div>
       </div>
-      <span v-if="!loading">{{ nextday }} 消防新聞</span>
+      <p v-if="!loading">{{ nextday }} 消防新聞</p>
+      <span v-if="items3.length == 0">待搜集</span>
       <ol>
         <li v-for="item in items3 | orderBy 'time'">
-          <a v-if="!hideTextLink" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.originLink }}" target="_blank">{{ item.title }}</a><span v-if="hideTextLink">{{ item.title }}</span>（{{ item.source }}）<span v-if="showTime">{{ item.timeText }}</span>
+          <a v-if="!hideTextLink" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.originLink }}" target="_blank">{{ item.title }}</a><span v-if="hideTextLink">{{ item.title }}</span>（{{ item.source }}）<span v-if="!hideTime">{{ item.timeText }}</span>
           <br/><a v-if="!hideShortUrl" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.link }}" target="_blank">{{ item.link }}</a>
         </li>
         <br />
         <p v-if="!loading">{{ nextday }} 消防新聞（續）</p>
         <li v-for="item in items4 | orderBy 'time'">
-          <a v-if="!hideTextLink" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.originLink }}" target="_blank">{{ item.title }}</a><span v-if="hideTextLink">{{ item.title }}</span>（{{ item.source }}）<span v-if="showTime">{{ item.timeText }}</span>
+          <a v-if="!hideTextLink" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.originLink }}" target="_blank">{{ item.title }}</a><span v-if="hideTextLink">{{ item.title }}</span>（{{ item.source }}）<span v-if="!hideTime">{{ item.timeText }}</span>
           <br/><a v-if="!hideShortUrl" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }"href="{{ item.link }}" target="_blank">{{ item.link }}</a>
         </li>
       </ol>
     </div>
   </div>
+
+  <div class="ui horizontal divider header">今日新竹市相關新聞</div>
+
+  <div class="ui piled segment fixed">
+    <div id="news">
+      <div v-if="loading2" class="ui active inverted dimmer">
+        <div class="ui large loader"></div>
+      </div>
+      <span v-if="items5.length == 0">今日無相關新聞</span>
+      <ol>
+        <li v-for="item in items5 | orderBy 'time'">
+          <a v-if="!hideTextLink" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.originLink }}" target="_blank">{{ item.title }}</a><span v-if="hideTextLink">{{ item.title }}</span>（{{ item.source }}）<span v-if="!hideTime">{{ item.timeText }}</span>
+          <br/><a v-if="!hideShortUrl" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }"href="{{ item.link }}" target="_blank">{{ item.link }}</a>
+        </li>
+      </ol>
+    </div>
+  </div>
+
+  <div class="ui horizontal divider header">今日低氣壓（颱風）相關新聞</div>
+
+  <div class="ui piled segment fixed">
+    <div id="news">
+      <div v-if="loading3" class="ui active inverted dimmer">
+        <div class="ui large loader"></div>
+      </div>
+      <span v-if="items6.length == 0">今日無相關新聞</span>
+      <ol>
+        <li v-for="item in items6 | orderBy 'time'">
+          <a v-if="!hideTextLink" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }" href="{{ item.originLink }}" target="_blank">{{ item.title }}</a><span v-if="hideTextLink">{{ item.title }}</span>（{{ item.source }}）<span v-if="!hideTime">{{ item.timeText }}</span>
+          <br/><a v-if="!hideShortUrl" v-bind:class="{ 'hide-link-underline': hideLinkUnderline }"href="{{ item.link }}" target="_blank">{{ item.link }}</a>
+        </li>
+      </ol>
+    </div>
+  </div>
+
   <small class="ui horizontal inverted divider header">
     lancetw&lt;at&gt;gmail.com, 2016
   </small>
@@ -70,7 +106,7 @@
 
 <script>
 import moment from 'moment'
-import { sortBy, pickBy } from 'lodash'
+import { sortBy, pickBy, isEmpty } from 'lodash'
 
 const serverAddress = process.env.API_SERVER
 
@@ -82,22 +118,45 @@ export default {
       const items1 = pickBy(rdata, (o) => {
         return moment(o.time).isBetween(moment().subtract(1, 'day').hour(17).minute(55), moment().hour(5).minute(55), 'minute', '[)')
       })
+      if (!isEmpty(items1)) this.items1 = items1
       const items2 = pickBy(rdata, (o) => {
         return moment(o.time).isBetween(moment().hour(5).minute(55), moment().hour(17).minute(55), 'minute', '[)')
       })
+      if (!isEmpty(items2)) this.items2 = items2
       const items3 = pickBy(rdata, (o) => {
         return moment(o.time).isBetween(moment().hour(17).minute(55), moment().add(1, 'day').hour(5).minute(55), 'minute', '[)')
       })
+      if (!isEmpty(items3)) this.items3 = items3
       const items4 = pickBy(rdata, (o) => {
         return moment(o.time).isBetween(moment().add(1, 'day').hour(5).minute(55), moment().add(1, 'day').hour(17).minute(55), 'minute', '[)')
       })
-
-      this.items1 = items1
-      this.items2 = items2
-      this.items3 = items3
-      this.items4 = items4
+      if (!isEmpty(items4)) this.items4 = items4
 
       this.loading = false
+    }, (errors) => {
+      console.log(errors)
+    })
+
+    this.loading2 = true
+    this.$http.get(serverAddress + '/api/news/v1/city').then((response) => {
+      const rdata = sortBy(response.data.news, (o) => { return o.time })
+      this.items5 = pickBy(rdata, (o) => {
+        return moment(o.time).isBetween(moment().hour(0).minute(0), moment().hour(23).minute(59), 'minute', '[)')
+      })
+
+      this.loading2 = false
+    }, (errors) => {
+      console.log(errors)
+    })
+
+    this.loading3 = true
+    this.$http.get(serverAddress + '/api/news/v1/typhon').then((response) => {
+      const rdata = sortBy(response.data.news, (o) => { return o.time })
+      this.items5 = pickBy(rdata, (o) => {
+        return moment(o.time).isBetween(moment().hour(0).minute(0), moment().hour(23).minute(59), 'minute', '[)')
+      })
+
+      this.loading3 = false
     }, (errors) => {
       console.log(errors)
     })
@@ -105,7 +164,9 @@ export default {
   data () {
     return {
       loading: false,
-      showTime: false,
+      loading2: false,
+      loading3: false,
+      hideTime: false,
       hideLinkUnderline: false,
       hideShortUrl: false,
       hideTextLink: false,
@@ -114,7 +175,9 @@ export default {
       items1: [],
       items2: [],
       items3: [],
-      items4: []
+      items4: [],
+      items5: [],
+      items6: []
     }
   }
 }
@@ -125,25 +188,32 @@ export default {
 h2 {
   font-size: 14pt;
   font-weight: normal;
-  line-height: 14pt;
+  line-height: auto;
   margin: 0;
 }
 
 #news {
   font-family: "標楷體", DFKai-SB, BiauKai, STKaiti;
   font-size: 14pt;
+  text-align: center;
+  color: #000;
+}
+
+#news p {
   text-align: left;
 }
 
 #news ol {
   margin: 0;
   padding: 0;
+  line-height: auto;
+  text-align: left;
 }
 
 #news ol li {
   margin: 0;
   padding: 0;
-  line-height: 14pt;
+  line-height: auto;
 }
 
 #news ol li a {
