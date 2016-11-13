e58ec0b2077f063ccf71dfbb82bafe18b85212af
lancetw aka Hsin-lin Cheng <lancetw@gmail.com>
2016-11-14 00:47:25 +0800

    updated

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <h1 class="ui header inverted">新竹市消防局 勤務派遣科 新聞資訊彙整</h1>
     <small class="ui inverted header">
-      version 5.0
+      version 5.1
     </small>
     <news></news>
   </div>
